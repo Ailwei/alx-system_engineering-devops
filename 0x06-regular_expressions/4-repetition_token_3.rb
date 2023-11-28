0cce6b38e96010fe7741bbe02beb835366539325
Ailwei <Ailweimaemu@gmail.com>
2023-11-28 05:39:41 -0800

    4. Repetition Token #3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the argument from the command line
-input_string = ARGV[0]
-
-# Define the regular expression to match the specified cases without square brackets
-regex = /hbt*n/
-
-# Use the scan method to find matches and join the results
-matches = input_string.scan(regex).join
-
-# Print the result
-puts matches
+puts ARGV[0].scan(/hbt*n/).join
