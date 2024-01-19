529c7cbd0d8432deb66e3dd8ddda8fc77f7b3d37
Ailwei <Ailweimaemu@gmail.com>
2024-01-19 14:54:30 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-#!/usr/bin/pup
-# File: 0-create_a_file.pp
+# A puppet manifest that creates a file school in /tmp.
+# The file (School) has permissions 0744, belonging to the user and group www-data.
 
 file { '/tmp/school':
-  ensure  => present,
-  content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   =>  'www-data',
+  ensure  => present,         # Ensure that the file exists
+  mode    => '0744',          # Set the permissions to 0744
+  owner   => 'www-data',      # Set the owner to www-data
+  group   => 'www-data',      # Set the group to www-data
+  content => 'I love Puppet', # Set the content of the file
 }
