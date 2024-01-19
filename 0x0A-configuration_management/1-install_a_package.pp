890f092a5b8679c1e9a411b26e5b67cf078e9053
Ailwei <Ailweimaemu@gmail.com>
2024-01-19 14:38:35 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
+#!/usr/bin/pup
+
 # File: 1-install_a_package.pp
 
+package { 'python3-pip':
+  ensure => installed,
+}
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip,
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+package { 'werkeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
