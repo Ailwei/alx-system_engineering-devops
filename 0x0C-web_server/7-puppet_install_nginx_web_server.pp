39b881c605b8a8dc93789ba5ad6ee482197e96cc
Ailwei <Ailweimaemu@gmail.com>
2024-01-23 04:24:04 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,53 +1,24 @@
-# Puppet manifest to install Nginx and configure it
+# Script to install nginx using puppet
 
-# Install Nginx package
-package { 'nginx':
+package {'nginx':
   ensure => 'present',
 }
 
-# Update package repositories
-exec { 'apt-update':
-  command => 'sudo apt-get update',
-  path    => '/usr/bin',
-  require => Package['nginx'],
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-# Install Nginx
-exec { 'install-nginx':
-  command  => 'sudo apt-get -y install nginx',
-  path     => '/usr/bin',
-  require  => Exec['apt-update'],
-  creates  => '/etc/nginx',
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+  provider => shell,
 }
 
-# Create Hello World index.html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Exec['install-nginx'],
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
 }
 
-# Configure Nginx for redirect
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Exec['install-nginx'],
-  notify  => Exec['nginx-restart'],
-}
-
-# Enable the redirect
-nginx::resource::location { '/redirect_me':
-  ensure   => present,
-  location => '^/redirect_me',
-  target   => 'https://www.youtube.com/',
-  status   => 'permanent',
-  require  => File['/etc/nginx/sites-available/default'],
-  notify   => Exec['nginx-restart'],
-}
-
-# Restart Nginx service
-exec { 'nginx-restart':
-  command => 'sudo service nginx restart',
-  path    => '/usr/bin',
-  require => [Exec['install-nginx'], File['/etc/nginx/sites-available/default']],
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
