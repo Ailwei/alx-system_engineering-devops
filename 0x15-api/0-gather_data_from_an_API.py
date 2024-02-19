93eafe38ec4bf20a1ed8ea5b7fc40a55fd75255e
Ailwei <Ailweimaemu@gmail.com>
2024-02-19 02:25:49 -0800

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,20 +3,13 @@
 Python script that, using this REST API,
 for a given employee ID, returns information about his/her TODO list progress.
 """
+
+
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
-    """
-    Retrieve information about the employee's TODO list progress.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        tuple: A tuple containing the employee name, number of completed tasks,
-               total number of tasks, and a list of completed task titles.
-    """
     # Fetch user data
     user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     if user_response.status_code != 200:
@@ -40,18 +33,10 @@ def get_employee_todo_progress(employee_id):
     return employee_name, completed_tasks, total_tasks, done_tasks
 
 def display_employee_todo_progress(employee_name, completed_tasks, total_tasks, done_tasks):
-    """
-    Display the employee's TODO list progress.
-
-    Args:
-        employee_name (str): The name of the employee.
-        completed_tasks (int): The number of completed tasks.
-        total_tasks (int): The total number of tasks.
-        done_tasks (list): A list of completed task titles.
-    """
     print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
     for task_title in done_tasks:
         print("\t", task_title)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
