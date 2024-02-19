7f98782a28c7e0514187c17ea7f526f8edbe6d3f
Ailwei <Ailweimaemu@gmail.com>
2024-02-19 02:11:51 -0800

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,13 +5,15 @@ using a REST API.
 
 Requirements:
 - The script must accept an integer as a parameter, which is the employee ID.
-- The script must display on the standard output the employee TODO list progress
+- Script must display on the standard output the employee TODO list progress
   in the following format:
-    - First line: "Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):"
+    - First line: "Employee EMPLOYEE_NAME is done with taskS"
       - EMPLOYEE_NAME: name of the employee
       - NUMBER_OF_DONE_TASKS: number of completed tasks
-      - TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-    - Second and N next lines display the title of completed tasks: "TASK_TITLE" (with 1 tabulation and 1 space before the TASK_TITLE)
+      - TOTAL_NUMBER_OF_TASKS: total number of tasks,
+      which is the sum of completed and non-completed tasks
+    - Second and N next lines display the title of completed tasks:
+    "TASK_TITLE"
 """
 
 import requests
@@ -25,7 +27,10 @@ if __name__ =="__main__":
     employee_id = int(sys.argv[1])
 
     # Fetch user data
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user_response = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id
+                )
+            )
     if user_response.status_code != 200:
         print("Error: Unable to fetch user data")
         sys.exit(1)
@@ -33,7 +38,10 @@ if __name__ =="__main__":
     employee_name = user_data.get('name')
 
     # Fetch TODO list data
-    todo_response = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+    todo_response = requests.get
+    (
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+            )
     if todo_response.status_code != 200:
         print("Error: Unable to fetch TODO list data")
         sys.exit(1)
@@ -45,6 +53,10 @@ if __name__ =="__main__":
     done_tasks = [task['title'] for task in todo_data if task['completed']]
 
     # Display progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+    print(
+            "Employee {} is done with tasks({}/{}):".format(
+                employee_name, completed_tasks, total_tasks
+                )
+            )
     for task in done_tasks:
         print("\t", task)
