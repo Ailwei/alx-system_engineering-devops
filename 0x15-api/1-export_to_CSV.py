52339ed74710cbe69cc6bfef55acc870ba34383a
Ailwei <Ailweimaemu@gmail.com>
2024-02-19 03:27:51 -0800

    1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Python script to export data in CSV format,
+you can use the csv module in Python.
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+
+    # Fetch user data
+    user_response = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(employee_id)
+            )
+    if user_response.status_code != 200:
+        print("Error: Unable to fetch user data")
+        sys.exit(1)
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
+
+    # Fetch TODO list data
+    todo_response = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(employee_id)
+            )
+    if todo_response.status_code != 200:
+        print("Error: Unable to fetch TODO list data")
+        sys.exit(1)
+        todo_data = todo_response.json()
+
+    # Process TODO list progress
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task['completed'])
+    done_tasks = [task['title'] for task in todo_data if task['completed']]
+
+    # Display progress
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
+    for task in done_tasks:
+        print("\t", task)
+
+    # Export data to csv file
+    csv_filename = '{}.csv'.format(employeed_id)
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([
+            'USER_ID',
+            'USERNAME',
+            'TASK_COMPLETED_STATUS',
+            'TASK_TITLE'
+            ]
+            )
+
+        for task in todo_data:
+            csv_writer.writerow([
+                employee_id,
+                employee_name,
+                task['completed'],
+                task['title']
+                ]
+                )
+
+    print("Data exported to:", csv_filename)
