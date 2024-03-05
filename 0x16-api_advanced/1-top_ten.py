238b0e4e02dd836fbecc8f04abf693c7a768ee85
Ailwei <Ailweimaemu@gmail.com>
2024-03-05 03:15:57 -0800

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ prints the titles of the first 10 hot posts listed for a given subreddit
 """
 import requests
 
+
 def top_ten(subreddit):
     """
     Function that queries the Reddit API and prints the titles of the first 10
