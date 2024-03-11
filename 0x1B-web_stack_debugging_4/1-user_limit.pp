cf3455e350f80769d937c74007b956f696d4cd94
Ailwei <Ailweimaemu@gmail.com>
2024-03-10 23:48:37 -0700

    1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Ensure holberton user exists and adjust open files limit
+user { 'holberton':
+  ensure => present,
+}
+
+# Adjusting open files limit for the holberton user
+exec { 'increase-holberton-open-files-limit':
+  command => 'echo "holberton hard nofile 10000" >> /etc/security/limits.conf && echo "holberton soft nofile 10000" >> /etc/security/limits.conf',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => 'grep -q "^holberton.*nofile.*10000" /etc/security/limits.conf',
+}
